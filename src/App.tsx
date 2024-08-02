11382b8394acd013cb253a5f6c256bb99b2ba558
edmilton75 <edmilton7500@gmail.com>
2024-08-02 19:36:28 -0300

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#101628",
+        display: "flex",
+        flexDirection: "column",
+
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <div>
+        <img
+          src="https://wallpapers.com/images/hd/the-avengers-superhero-movie-eeotwqkmypkvalg9.webp"
+          alt=""
+          style={{ height: "30rem" }}
+        />
+      </div>
+
+      <header></header>
+    </div>
+  );
+};
+
+export default App;
